2ca47576367fe147ce450ce178a52eddaf2dc8e7
Ludvig Hult <ludvig.hult@gmail.com>
2021-02-05 15:33:57 +0100

    added some caching to increase speed slightly

--- lib/linear_algebra.py
+++ lib/linear_algebra.py
@@ -1,12 +1,16 @@
+from functools import lru_cache
 import itertools
 import numpy as np
 
 
+@lru_cache
 def make_L_no_diag(d_nodes_in_graph):
     """
     The parametrization matrix L so that
         L@v = vecop(W)
     with W having zeros on the diagonal
+
+    N.B. this is cached, so don't modify it in place!
     """
     d2 = d_nodes_in_graph ** 2
     n_vars = d_nodes_in_graph * (d_nodes_in_graph - 1)
@@ -21,3 +25,12 @@ def make_L_no_diag(d_nodes_in_graph):
             L[L_row, L_col] = 1
             L_col += 1
     return L
+
+
+@lru_cache
+def make_Z_clear_first(d):
+    """Make an Identity with zero on first row
+    N.B. this is cached, so don't modify it in place!"""
+    Z = np.eye(d)
+    Z[0, 0] = 0
+    return Z
