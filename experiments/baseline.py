87b56d8cc0e9cdeba3de2deddc8eb7df65b3d0fa
Ludvig Hult <ludvig.hult@gmail.com>
2021-02-17 17:51:15 +0100

    added avg ace in output for baseline experiment

--- experiments/baseline.py
+++ experiments/baseline.py
@@ -268,6 +268,7 @@ def post_process(output_folder):
         lambda x: f"{x['p']:7.2%} ({x['pmin']:7.2%},{x['pmax']:7.2%})", axis=1
     ).to_frame("CR")
     avg_w = df.groupby(dimensions)["ci_width"].mean().to_frame("Avg CI width")
+    avg_w = df.groupby(dimensions)["ace"].mean().to_frame("Avg ACE est")
     summary = pd.concat([cr_nice, avg_w], axis=1)
 
     summ_path = output_folder.joinpath(summary_fname)
